bf707814ddef3486ce3437ba22362b729bcf7c57
rit-rit <rit80rit@gmail.com>
2018-07-03 22:16:19 +0900

    Change formatted files

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,4 @@
-import Vue from "vue";
-import App from "../components/App.vue";
+import Vue from 'vue';
+import App from 'src/components/App.vue';
 
-new Vue({ el: "#app", components: { App } });
+new Vue({ el: '#app', components: { App }});
