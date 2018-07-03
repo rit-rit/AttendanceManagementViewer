fc61bb01d684ba52ff3910960105e46eab49118a
rit-rit <rit80rit@gmail.com>
2018-07-03 22:16:19 +0900

    Change formatted files

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Components from 'vue-class-component'
+import Vue from "vue";
+import Components from "vue-class-component";
 @Components
 export default class App extends Vue {
-  message: string = 'Hello World!!'
+  message: string = "Hello World!!";
 }
 </script>
