a577267b671ebf4899585dadc19cc8e9373dda6a
rit-rit <rit80rit@gmail.com>
2018-07-09 23:24:08 +0900

    Add AttendanceViewerItem

--- src/components/AttendanceViewer.vue
+++ src/components/AttendanceViewer.vue
@@ -1,12 +1,22 @@
 <template>
-  <span>Here is AttendanceViewer</span>
+  <div>
+    <span>Here is AttendanceViewer</span>
+    <br>
+    <AttendanceViewerItem></AttendanceViewerItem>
+    <br>
+    <AttendanceViewerItem></AttendanceViewerItem>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
 import Component from "vue-class-component";
+import AttendanceViewerItem from "src/components/AttendanceViewerItem.vue"
 
-@Component
+@Component({
+  components:{AttendanceViewerItem}
+  }
+)
 export default class AttendanceViewer extends Vue{
 
 }
