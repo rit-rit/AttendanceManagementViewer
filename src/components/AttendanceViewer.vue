fa9ca3f27f69be55005b37b95c6fe2307f4eca80
rit-rit <rit80rit@gmail.com>
2018-07-14 12:06:30 +0900

    Create temporary branch

--- src/components/AttendanceViewer.vue
+++ src/components/AttendanceViewer.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    <span>Here is AttendanceViewer</span>
-    <br>
-    <AttendanceViewerItem></AttendanceViewerItem>
-    <br>
-    <AttendanceViewerItem></AttendanceViewerItem>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Attendance Division</th>
+        <th>Start Time</th>
+        <th>End Time</th>
+        <th>Rest Time</th>
+        <th>Work Time</th>
+        <th>Overtime (Weekday)</th>
+        <th>Late-night overtime (Weekday)</th>
+        <th id="AAA" @click="switchDisplay">Overtime (Holiday)</th>
+        <th v-if="overtimeHolidayShowFlag">Late-night overtime (Holiday)</th>
+        <th>Holiday work</th>
+        <th>Holiday work (overtime)</th>
+        <th>Compensatory day off</th>
+        <th>Substitution holiday work </th>
+        <th>Substitution holiday work (overtime)</th>
+        <th>e-work</th>
+        <th>Note</th>
+      </tr>
+    </thead>
+    <tbody>
+      <AttendanceViewerItem v-bind:isShown="overtimeHolidayShowFlag"></AttendanceViewerItem>
+      <AttendanceViewerItem v-bind:isShown="overtimeHolidayShowFlag"></AttendanceViewerItem>
+    </tbody>
+  </table>
 </template>
 
 <script lang="ts">
@@ -16,7 +36,26 @@ import AttendanceViewerItem from 'src/components/AttendanceViewerItem.vue';
 @Component({
   components: { AttendanceViewerItem }
 })
-export default class AttendanceViewer extends Vue {}
+export default class AttendanceViewer extends Vue {
+  overtimeHolidayShowFlag: boolean = true;
+  switchDisplay(event: Event): void {
+    this.overtimeHolidayShowFlag = !this.overtimeHolidayShowFlag;
+  }
+}
 </script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+  border: 2px black solid;
+}
+th {
+  border: 1px red solid;
+}
+thead {
+  border-bottom: 3px double green;
+  color: red;
+}
+</style>
 
 
--- src/components/AttendanceViewerItem.vue
+++ src/components/AttendanceViewerItem.vue
@@ -1,13 +1,41 @@
 <template>
-  <span>Here is AttendanceViewerItem</span>
+  <tr>
+    <th>2018/01/01</th>
+    <th>Attendance Division</th>
+    <th><input type="text"></th>
+    <th>End Time</th>
+    <th>Rest Time</th>
+    <th>Work Time</th>
+    <th>Overtime (Weekday)</th>
+    <th>Late-night overtime (Weekday)</th>
+    <th>Overtime (Holiday)</th>
+    <th v-if="isShown">Late-night overtime (Holiday)</th>
+    <th>Holiday work</th>
+    <th>Holiday work (overtime)</th>
+    <th>Compensatory day off</th>
+    <th>Substitution holiday work </th>
+    <th>Substitution holiday work (overtime)</th>
+    <th>e-work</th>
+    <th>Note</th>
+  </tr>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
 
 @Component
-export default class AttendanceViewerItem extends Vue {}
+export default class AttendanceViewerItem extends Vue {
+  @Prop() isShown: boolean = true;
+}
 </script>
+<style scoped>
+tr,
+th {
+  border: 1px red solid;
+}
+</style>
+
 
 
