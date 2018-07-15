27242b7f9403ec677532e51151b64887aaa7d798
rit-rit <rit80rit@gmail.com>
2018-07-15 22:25:25 +0900

    Add table layout

--- src/components/AttendanceViewer.vue
+++ src/components/AttendanceViewer.vue
@@ -10,7 +10,8 @@
         <th>Work Time</th>
         <th>Overtime (Weekday)</th>
         <th>Late-night overtime (Weekday)</th>
-        <th id="AAA" @click="switchDisplay">Overtime (Holiday)</th>
+        <th v-if="!overtimeHolidayShowFlag" @click="switchDisplay">Overtime (Holiday)(+)</th>
+        <th id="AAA" v-if="overtimeHolidayShowFlag" @click="switchDisplay">Overtime (Holiday)(-)</th>
         <th v-if="overtimeHolidayShowFlag">Late-night overtime (Holiday)</th>
         <th>Holiday work</th>
         <th>Holiday work (overtime)</th>
@@ -22,8 +23,7 @@
       </tr>
     </thead>
     <tbody>
-      <AttendanceViewerItem v-for="(dateItem,index) in dateArray" :key="index" v-bind:date="dateItem" v-bind:isShown="overtimeHolidayShowFlag"></AttendanceViewerItem>
-      <!--<AttendanceViewerItem v-bind:isShown="overtimeHolidayShowFlag"></AttendanceViewerItem>-->
+      <AttendanceViewerItem v-for="(dateItem,index) in dateArray" :key="index" v-bind:dateProp="dateItem" v-bind:isShown="overtimeHolidayShowFlag"></AttendanceViewerItem>
     </tbody>
   </table>
 </template>
@@ -36,7 +36,7 @@ import AttendanceViewerItem from 'src/components/AttendanceViewerItem.vue';
   components: { AttendanceViewerItem }
 })
 export default class AttendanceViewer extends Vue {
-  dateArray: string[] = [];
+  dateArray: Date[] = [];
   overtimeHolidayShowFlag: boolean = true;
 
   mounted(): void {
@@ -46,13 +46,9 @@ export default class AttendanceViewer extends Vue {
       currentDate.getMonth() + 1,
       0
     ).getDate();
-    for (var curDay: number = 2; curDay <= lastDay + 1; curDay++) {
+    for (var curDay: number = 1; curDay <= lastDay + 1; curDay++) {
       this.dateArray.push(
-        new Date(
-          currentDate.getFullYear(),
-          currentDate.getMonth(),
-          curDay
-        ).toDateString()
+        new Date(currentDate.getFullYear(), currentDate.getMonth(), curDay)
       );
     }
   }
@@ -68,10 +64,10 @@ table {
   border: 2px black solid;
 }
 th {
-  border: 1px red solid;
+  border: 1px black solid;
 }
 thead {
-  border-bottom: 3px double green;
+  border-bottom: 3px black double;
   color: red;
 }
 th.date {
--- src/components/AttendanceViewerItem.vue
+++ src/components/AttendanceViewerItem.vue
@@ -1,13 +1,39 @@
 <template>
-  <span>Here is AttendanceViewerItem</span>
+  <tr>
+    <th>{{dateProp.toDateString()}}</th>
+    <th>Attendance Division</th>
+    <th><input type="text"></th>
+    <th>End Time</th>
+    <th>Rest Time</th>
+    <th>Work Time</th>
+    <th>Overtime (Weekday)</th>
+    <th>Late-night overtime (Weekday)</th>
+    <th v-if="!isShown">...</th>
+    <th v-if="isShown">Overtime (Holiday)</th>
+    <th v-if="isShown">Late-night overtime (Holiday)</th>
+    <th>Holiday work</th>
+    <th>Holiday work (overtime)</th>
+    <th>Compensatory day off</th>
+    <th>Substitution holiday work </th>
+    <th>Substitution holiday work (overtime)</th>
+    <th>e-work</th>
+    <th>Note</th>
+  </tr>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-
+import { Prop, Provide } from 'vue-property-decorator';
 @Component
-export default class AttendanceViewerItem extends Vue {}
+export default class AttendanceViewerItem extends Vue {
+  @Prop() isShown: boolean = true;
+  @Prop(Date) dateProp!: Date;
+}
 </script>
-
-
+<style scoped>
+tr,
+th {
+  border: 1px black solid;
+}
+</style>
