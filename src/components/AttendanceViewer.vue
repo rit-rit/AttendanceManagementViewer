11b15dd4f8bbccad639687251ab6023f5bbf92a1
rit-rit <rit80rit@gmail.com>
2018-07-27 23:47:00 +0900

    Add mocked axios post code

--- src/components/AttendanceViewer.vue
+++ src/components/AttendanceViewer.vue
@@ -1,37 +1,41 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th class="date">Date</th>
-        <th>Attendance Division</th>
-        <th>Start Time</th>
-        <th>End Time</th>
-        <th>Rest Time</th>
-        <th>Work Time</th>
-        <th>Overtime (Weekday)</th>
-        <th>Late-night overtime (Weekday)</th>
-        <th v-if="!isShownOvertimeHoliday" @click="switchShownOvertimeHoliday">Overtime (Holiday)(+)</th>
-        <th v-if="isShownOvertimeHoliday" @click="switchShownOvertimeHoliday">Overtime (Holiday)(-)</th>
-        <th v-if="isShownOvertimeHoliday">Late-night overtime (Holiday)</th>
-        <th>Holiday work</th>
-        <th>Holiday work (overtime)</th>
-        <th>Compensatory day off</th>
-        <th>Substitution holiday work </th>
-        <th>Substitution holiday work (overtime)</th>
-        <th>e-work</th>
-        <th>Note</th>
-      </tr>
-    </thead>
-    <tbody>
-      <AttendanceViewerItem v-for="(dateItem,index) in dateArray" :key="index" v-bind:dateProp="dateItem" v-bind:isShownOvertimeHolidayProp="isShownOvertimeHoliday"></AttendanceViewerItem>
-    </tbody>
-  </table>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th class="date">Date</th>
+          <th>Attendance Division</th>
+          <th>Start Time</th>
+          <th>End Time</th>
+          <th>Rest Time</th>
+          <th>Work Time</th>
+          <th>Overtime (Weekday)</th>
+          <th>Late-night overtime (Weekday)</th>
+          <th v-if="!isShownOvertimeHoliday" @click="switchShownOvertimeHoliday">Overtime (Holiday)(+)</th>
+          <th v-if="isShownOvertimeHoliday" @click="switchShownOvertimeHoliday">Overtime (Holiday)(-)</th>
+          <th v-if="isShownOvertimeHoliday">Late-night overtime (Holiday)</th>
+          <th>Holiday work</th>
+          <th>Holiday work (overtime)</th>
+          <th>Compensatory day off</th>
+          <th>Substitution holiday work </th>
+          <th>Substitution holiday work (overtime)</th>
+          <th>e-work</th>
+          <th>Note</th>
+        </tr>
+      </thead>
+      <tbody>
+        <AttendanceViewerItem v-for="(dateItem,index) in dateArray" :key="index" v-bind:dateProp="dateItem" v-bind:isShownOvertimeHolidayProp="isShownOvertimeHoliday"></AttendanceViewerItem>
+      </tbody>
+    </table>
+    <button @click="postDate">Post</button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import AttendanceViewerItem from 'src/components/AttendanceViewerItem.vue';
+import axios from 'axios';
 @Component({
   components: { AttendanceViewerItem }
 })
@@ -54,6 +58,17 @@ export default class AttendanceViewer extends Vue {
   }
   switchShownOvertimeHoliday(event: Event): void {
     this.isShownOvertimeHoliday = !this.isShownOvertimeHoliday;
+  }
+
+  postDate(): void {
+    axios
+      .post('http://localhost:8081/dummy', { id: 'AAAA' })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 }
 </script>
