9ec257ae3f3b74e644a3d6bf4d4b7c63aa9d882d
rit-rit <rit80rit@gmail.com>
2018-07-31 23:20:07 +0900

    Add AttendanceItem class

---
+++ src/ts/AttendanceItem.ts
@@ -0,0 +1,11 @@
+class AttendanceItem {
+  private date: string;
+  private startTime: string;
+  private endTime: string;
+
+  constructor() {
+    this.date = '';
+    this.startTime = '';
+    this.endTime = '';
+  }
+}
