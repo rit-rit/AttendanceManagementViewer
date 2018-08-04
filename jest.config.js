028ea0019f1ae1e7f638e1ad3d4f3dd452f9f7ed
rit-rit <rit80rit@gmail.com>
2018-08-04 21:20:50 +0900

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  moduleFileExtensions: ['vue', 'js', 'ts'],
+  testMatch: ['<rootDir>/test/unit/specs/**/*.ts'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  collectCoverageFrom: ['<rootDri>/src/components/**/*.vue'],
+  coverageDirectory: '<rootDir>/test/unit/coverage',
+  coverageReporters: ['html', 'test-summary'],
+  verbose: true,
+  testURL: 'http://localhost/'
+};
