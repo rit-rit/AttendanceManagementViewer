970fbe6264dedc68c7a62b943647e1b78af137b4
rit-rit <rit80rit@gmail.com>
2018-08-10 21:52:22 +0900

    Add unit test

--- test/unit/specs/components/TestAttendanceViewerItem.spec.ts
+++ test/unit/specs/components/TestAttendanceViewerItem.spec.ts
@@ -1,18 +1,32 @@
 import AttendanceViewerItem from 'src/components/AttendanceViewerItem.vue';
 import { mount } from '@vue/test-utils';
 
-test('mounted function test', () => {
+test('mounted function test(Holoday)', () => {
+  // 2000/1/1 00:00:00.00(Sat)
+  const date: Date = new Date(2000, 0, 1, 0, 0, 0, 0);
+  const wrapper = mount(AttendanceViewerItem, {
+    propsData: { isShownOvertimeHolidayProp: true, dateProp: date }
+  });
+  expect(wrapper.vm.attendanceDivision).toBe('holiday');
+});
+
+test('mounted function test(Weekday)', () => {
+  // 2000/1/3 00:00:00.00(Mon)
+  const date: Date = new Date(2000, 0, 3, 0, 0, 0, 0);
+  const wrapper = mount(AttendanceViewerItem, {
+    propsData: { isShownOvertimeHolidayProp: true, dateProp: date }
+  });
+  expect(wrapper.vm.attendanceDivision).toBe('work');
+});
+
+test('workTime function test', () => {
   const date: Date = new Date();
   const wrapper = mount(AttendanceViewerItem, {
     propsData: { isShownOvertimeHolidayProp: true, dateProp: date }
   });
-  // console.log('=====');
   wrapper.setData({ startTime: '9:00' });
   wrapper.setData({ endTime: '18:00' });
   wrapper.setData({ restTime: '1:00' });
-  // console.log(wrapper.props().dateProp.toString());
-  // console.log(wrapper.props().isShownOvertimeHolidayProp);
 
   expect(wrapper.vm.workTime).toBe('08:00');
-  console.log('=====');
 });
