a0a908ffdc0ed1f3af424c586ea4718a73df92da
rit-rit <rit80rit@gmail.com>
2018-09-06 14:37:29 +0900

    Modify unit test

--- test/unit/specs/components/TestAttendanceViewer.spec.ts
+++ test/unit/specs/components/TestAttendanceViewer.spec.ts
@@ -5,19 +5,11 @@ test('mounted function test', () => {
   const wrapper = shallowMount(AttendanceViewer);
 
   // First item of dateArray is the first date of current month.
-  expect(wrapper.vm.dateArray[0].getDate()).toEqual(1);
+  expect(wrapper.vm.attendanceArray[0].date.getDate()).toEqual(1);
   // Last item of dateArray is the Last date of current month.
   expect(
-    wrapper.vm.dateArray[wrapper.vm.dateArray.length - 1].getDate()
-  ).toEqual(wrapper.vm.dateArray.length);
+    wrapper.vm.attendanceArray[
+      wrapper.vm.attendanceArray.length - 1
+    ].date.getDate()
+  ).toEqual(wrapper.vm.attendanceArray.length);
 });
-
-test('switchShowOvertimeHoliday function test.', () => {
-  const wrapper = shallowMount(AttendanceViewer);
-  expect(wrapper.vm.isShownOvertimeHoliday).toBeTruthy();
-});
-
-test("postAttendanceData function test.",()=>{
-  const wrapper = shallowMount(AttendanceViewer);
-  expect(wrapper.vm.postAttendanceData()).toBeFalsy();
-})
