d96c31052cafb074161cf18842cdc76b9153bc70
rit-rit <rit80rit@gmail.com>
2018-09-11 16:12:53 +0900

    Modify uni test

--- test/unit/specs/components/TestAttendanceViewerItem.spec.ts
+++ test/unit/specs/components/TestAttendanceViewerItem.spec.ts
@@ -4,11 +4,11 @@ import Vue from 'vue';
 
 test('mounted function test(Holoday)', () => {
   // 2000/1/1 00:00:00.00(Sat)
-  const date: Date = new Date(2000, 0, 1, 0, 0, 0, 0);
+  const tmpdate: Date = new Date(2000, 0, 1, 0, 0, 0, 0);
   const wrapper = mount(AttendanceViewerItem, {
     propsData: {
       index: 0,
-      date,
+      date: tmpdate,
       isShownOvertimeHoliday: false,
       startTime: '',
       endTime: '',
@@ -22,11 +22,11 @@ test('mounted function test(Holoday)', () => {
 
 test('mounted function test(Weekday)', () => {
   // 2000/1/3 00:00:00.00(Mon)
-  const date: Date = new Date(2000, 0, 3, 0, 0, 0, 0);
+  const tmpdate: Date = new Date(2000, 0, 3, 0, 0, 0, 0);
   const wrapper = mount(AttendanceViewerItem, {
     propsData: {
       index: 0,
-      date,
+      date: tmpdate,
       isShownOvertimeHoliday: false,
       startTime: '',
       endTime: '',
@@ -39,11 +39,11 @@ test('mounted function test(Weekday)', () => {
 });
 
 test('workTime function test', () => {
-  const date: Date = new Date();
+  const tmpdate: Date = new Date();
   const wrapper = mount(AttendanceViewerItem, {
     propsData: {
       index: 0,
-      date,
+      date: tmpdate,
       isShownOvertimeHoliday: false,
       startTime: '9:00',
       endTime: '18:00',
@@ -53,4 +53,22 @@ test('workTime function test', () => {
     }
   });
   expect(wrapper.vm.workTime).toBe('08:00');
+});
+
+test('overtimeOnHoliday function test. The case is that the date is weekday.', () => {
+  // 2000/1/3 00:00:00.00(Mon)
+  const tmpdate: Date = new Date(2000, 0, 3, 0, 0, 0, 0);
+  const wrapper = mount(AttendanceViewerItem, {
+    propsData: {
+      index: 0,
+      date: tmpdate,
+      isShownOvertimeHoliday: false,
+      startTime: '9:00',
+      endTime: '18:00',
+      restTime: '1:00',
+      workTime: '',
+      attendanceDivision: 'work'
+    }
+  });
+  expect(wrapper.vm.overtimeOnHoliday).toBe('');
 });
